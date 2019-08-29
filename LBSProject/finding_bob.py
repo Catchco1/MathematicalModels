61dca6431b46801870aca7e4a1bf30e3b5df786e
Catchco1 <cag107@gmail.com>
2019-08-29 17:58:06 -0400

    Add spreadsheet and latest version of code

--- LBSProject/finding_bob.py
+++ LBSProject/finding_bob.py
@@ -4,13 +4,13 @@ from math import sqrt
 ## (x,y) from (a,b), (a-1,b) and (a,b-1), respectively.
 ## The last two parameters a and b are optional, both 0 by default.
 
-def bob_finder(d1,d2,d3,a=0,b=0):
+def bob_finder(d1,d2,d3,a,b):
     x = a + (d2**2 - d1**2 - 1)/2
     y = b + (d3**2 - d1**2 - 1)/2
 
     distance_error = abs(d1 - sqrt((x-a)**2 + (y-b)**2))
 
-    if distance_error < 10**(-8):
+    if distance_error < 10**(-20):
         print('Bob is at the point ({0:.3f},{1:.3f})'.format(x,y))
     else:
         print('Mismatch between distances is {0:.6f}'.format(distance_error))
@@ -18,8 +18,8 @@ def bob_finder(d1,d2,d3,a=0,b=0):
 
 
 ## Examples of use (remove comment and compile or copy into console):
-# bob_finder(sqrt(13),sqrt(20),sqrt(10))
+#bob_finder(405.3, 404.7, 406.1, 0, 0)
 ## or
-# bob_finder(sqrt(13),sqrt(20),sqrt(10),1,-2)
+bob_finder(35.2, 35.9, 34.5, -243.5, 324.06)
 ## or
 # bob_finder(sqrt(13),sqrt(20),sqrt(10),b=2,a=1)
