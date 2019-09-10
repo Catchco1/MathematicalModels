f8669dbc2c1d97ebe998d9ae3c2bb08ebf0dfd8a
Catchco1 <cag107@gmail.com>
2019-09-10 16:24:34 -0400

    Add more tests and additional y-coord calculations

--- LBSProject/debugging.py
+++ LBSProject/debugging.py
@@ -24,7 +24,9 @@ def locator(distance):
     xPrime = x
     xPrime2 = x
     yPrime = y
+    yPrime2 = y
     y1 = y
+    y2 = y
     while(distance(x1,y) < 1):
         x1 += 0.25
     while(distance(x2, y) < 1):
@@ -54,6 +56,8 @@ def locator(distance):
             x2 = c 
     while(distance(xPrime, y1) < 1):
         y1 += 0.25
+    while(distance(xPrime, y2) < 1):
+        y2 -= 0.25
 
     while (abs(yPrime - y1) >= 0.001): 
         # Find middle point 
@@ -66,16 +70,29 @@ def locator(distance):
             yPrime = c 
         else: 
             y1 = c
-    BobX = (xPrime + xPrime2) / 2
-    BobY = (yPrime + y) / 2
+    while(abs(yPrime2 - y2) >= 0.001):
+        # Find middle point 
+        c = (yPrime2+y2)/2
 
-    print(BobX, BobY)  
-    return((BobX,BobY),1)
+        check = distance(xPrime, c)
+   
+        # Decide the side to repeat the steps 
+        if (check == 0): 
+            yPrime2 = c 
+        else: 
+            y2 = c
+    BobX = (xPrime + xPrime2) / 2
+    BobY = (yPrime + yPrime2) / 2
+
+    print((BobX, BobY))  
+    return((round(BobX,2),round(BobY,2)),1)
     
 
 # To test:
 
-locator(dist(210.3124,342.3248))
+#locator(dist(210.3124, 342.3248))
+#locator(dist(210,-340))
+locator(dist(-20.5463,34.46372))
 #locator(dist(-270.68,301.76))
 
 
