93ccea5d4373895a2bedfc736a23cfb0e174ce28
Catchco1 <cag107@gmail.com>
2019-09-12 21:21:14 -0400

    Best version and create submission file

--- LBSProject/check_wrap.py
+++ LBSProject/check_wrap.py
@@ -1,6 +1,7 @@
 from math import sqrt
 from multiprocessing import Pool
 from time import time
+import random
 
 """
 Your goal is to write the function locator() that inputs the distance
@@ -36,19 +37,119 @@ function no more than 25 times.
 """
 
 
+# def locator(distance):
+#     (x,y) = (0,0)
+#     d = distance(x,y)
+#     while d > 0:
+#         x_new = x - (distance(x+1,y) - d)*d
+#         y_new = y - (distance(x,y+1) - d)*d
+#         if x_new == x and y_new == y:
+#             x_new = x + 1
+#         (x,y) = (x_new,y_new)
+#         print(x,y)
+#         d = distance(x,y)
+#     return((x,y),1)
 def locator(distance):
     (x,y) = (0,0)
     d = distance(x,y)
-    while d > 0:
-        x_new = x - (distance(x+1,y) - d)*d
-        y_new = y - (distance(x,y+1) - d)*d
+    count = 0
+    count += 1
+    #print('count for gradient:{}'.format(count))
+    while d > 1:
+        x_new = x - (distance(x+1.99,y) - d)*d/1.99
+        count += 1
+        #print('count for gradient:{}'.format(count))
+        y_new = y - (distance(x,y+1.99) - d)*d/1.99
+        count += 1
+        #print('count for gradient:{}'.format(count))
         if x_new == x and y_new == y:
-            x_new = x + 1
+            x_new = x + 1.99
         (x,y) = (x_new,y_new)
-        print(x,y)
+        #print(x,y)
         d = distance(x,y)
-    return((x,y),1)
+        count += 1
+    print('count for gradient:{}'.format(count))
+    print('d:{}'.format(d))
+    #return
+    count = 0
+    while(d > 0):
+        increment = 0.5
+        count += 1
+        #print('count for fix:{}'.format(count))
+        if(distance(x - 1000.5, y) > 1000):
+            count += 1
+            #print('count for fix:{}'.format(count))
+            x += increment #0.15
+        else:
+            count += 1
+            #print('count for fix:{}'.format(count))
+            x -= increment
+        if(distance(x, y - 1000.5) > 1000):
+            count += 1
+            y += increment
+        else:
+            count += 1
+            y -= increment
+        d = distance(x,y)
+    print('count from fix:{}'.format(count))
+    x1 = x + 1
+    x2 = x - 1
+    xPrime = x
+    xPrime2 = x
+    yPrime = y
+    while (abs(xPrime - x1) >= 0.01): 
+  
+        # Find middle point 
+        c = (xPrime+x1)/2
 
+        check = distance(c,y)
+        count += 1
+        #print('count for x1:{}'.format(count))
+   
+        # Decide the side to repeat the steps 
+        if (check == 0): 
+            xPrime = c 
+        else: 
+            x1 = c 
+    while(abs(xPrime2 - x2) >= 0.01):
+        # Find middle point 
+        c = (xPrime2+x2)/2
+
+        check = distance(c,y)
+        count += 1
+        #print('count for x2:{}'.format(count))
+   
+        # Decide the side to repeat the steps 
+        if (check == 0): 
+            xPrime2 = c 
+        else: 
+            x2 = c 
+    if(distance(xPrime2,y+0.1) == 0):
+        count += 1
+        #print('count for yStart:{}'.format(count))
+        y1 = y + 1
+    else:
+        count += 1
+        #print('count for yStart:{}'.format(count))
+        y1 = y - 1
+    while (abs(yPrime - y1) >= 0.01): 
+        # Find middle point 
+        c = (yPrime+y1)/2
+
+        check = distance(xPrime, c)
+        count += 1
+        #print('count for y1:{}'.format(count))
+   
+        # Decide the side to repeat the steps 
+        if (check == 0): 
+            yPrime = c 
+        else: 
+            y1 = c
+    BobX = (xPrime + xPrime2) / 2
+    BobY = (yPrime + y) / 2
+
+    print((BobX, BobY))  
+    return((round(BobX,2),round(BobY,2)),0.01)
 ### The code below will be used for grading the assignment.
 def locator_check(a,b):
     global count
@@ -70,7 +171,10 @@ def locator_check(a,b):
                                                       error,count,run_time))
 
 def main():
-    points = [[210,-340],[-20.5463,34.46372],[10,10],[0.25,0.25]]
+    #points = [[210,-340],[-20.5463,34.46372],[10,15],[5,5],[1,1],[-100,-100]]
+    points = []
+    for i in range(9):
+        points += [[round(random.uniform(-500,500),3),round(random.uniform(-500,500),3)]]
     check = Pool()
     for point in points:
         result = check.apply_async(locator_check,point)
--- LBSProject/finalLocator.py
+++ LBSProject/finalLocator.py
@@ -1,48 +1,46 @@
-from math import sqrt
-
-
-
-def dist(a,b):
-    return(lambda x,y : round(sqrt((x-a)**2+(y-b)**2)))
-
-
 def locator(distance):
     (x,y) = (0,0)
     d = distance(x,y)
     count = 0
     count += 1
-    print('count for gradient:{}'.format(count))
+    #print('count for gradient:{}'.format(count))
     while d > 1:
-        x_new = x - (distance(x+1,y) - d)*d/1
+        x_new = x - (distance(x+1.99,y) - d)*d/1.99
         count += 1
-        print('count for gradient:{}'.format(count))
-        y_new = y - (distance(x,y+1) - d)*d/1
+        #print('count for gradient:{}'.format(count))
+        y_new = y - (distance(x,y+1.99) - d)*d/1.99
         count += 1
-        print('count for gradient:{}'.format(count))
+        #print('count for gradient:{}'.format(count))
         if x_new == x and y_new == y:
-            x_new = x + 1
+            x_new = x + 1.99
         (x,y) = (x_new,y_new)
         #print(x,y)
         d = distance(x,y)
         count += 1
-        print('count for gradient:{}'.format(count))
+    print('count for gradient:{}'.format(count))
+    print('d:{}'.format(d))
     #return
+    count = 0
     while(d > 0):
+        increment = 0.5
         count += 1
-        print('count for fix:{}'.format(count))
+        #print('count for fix:{}'.format(count))
         if(distance(x - 1000.5, y) > 1000):
             count += 1
-            print('count for fix:{}'.format(count))
-            x += 0.25
+            #print('count for fix:{}'.format(count))
+            x += increment #0.15
         else:
             count += 1
-            print('count for fix:{}'.format(count))
-            x -= 0.25
+            #print('count for fix:{}'.format(count))
+            x -= increment
         if(distance(x, y - 1000.5) > 1000):
-            y += 0.25
+            count += 1
+            y += increment
         else:
-            y -= 0.25
+            count += 1
+            y -= increment
         d = distance(x,y)
+    print('count from fix:{}'.format(count))
     x1 = x + 1
     x2 = x - 1
     xPrime = x
@@ -55,7 +53,7 @@ def locator(distance):
 
         check = distance(c,y)
         count += 1
-        print('count for x1:{}'.format(count))
+        #print('count for x1:{}'.format(count))
    
         # Decide the side to repeat the steps 
         if (check == 0): 
@@ -68,7 +66,7 @@ def locator(distance):
 
         check = distance(c,y)
         count += 1
-        print('count for x2:{}'.format(count))
+        #print('count for x2:{}'.format(count))
    
         # Decide the side to repeat the steps 
         if (check == 0): 
@@ -77,11 +75,11 @@ def locator(distance):
             x2 = c 
     if(distance(xPrime2,y+0.1) == 0):
         count += 1
-        print('count for yStart:{}'.format(count))
+        #print('count for yStart:{}'.format(count))
         y1 = y + 1
     else:
         count += 1
-        print('count for yStart:{}'.format(count))
+        #print('count for yStart:{}'.format(count))
         y1 = y - 1
     while (abs(yPrime - y1) >= 0.01): 
         # Find middle point 
@@ -89,7 +87,7 @@ def locator(distance):
 
         check = distance(xPrime, c)
         count += 1
-        print('count for y1:{}'.format(count))
+        #print('count for y1:{}'.format(count))
    
         # Decide the side to repeat the steps 
         if (check == 0): 
@@ -101,13 +99,3 @@ def locator(distance):
 
     print((BobX, BobY))  
     return((round(BobX,2),round(BobY,2)),0.01)
-    
-
-# To test:
-
-locator(dist(210.3124, 342.3248))
-locator(dist(210,-340))
-locator(dist(-20.5463,34.46372))
-locator(dist(-270.68,301.76))
-locator(dist(10,10))
-locator(dist(0.25,0.25))
